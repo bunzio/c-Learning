c4c69192de2be6c9e305e13c62efb61552026f91
Bunzio <bunzeladam@gmail.com>
2022-03-15 22:04:12 +0100

    kk

---
+++ TestClasss/TestClasss/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using TestClasss;
+
+WritteHello p1 = new WritteHello(true);
+WritteHello p2 = new WritteHello(false);
+
+string p4 = Console.ReadLine();
+
+
---
+++ TestClasss/TestClasss/WritteHello.cs
@@ -0,0 +1,26 @@
+﻿namespace TestClasss;
+
+public class WritteHello
+{
+    void wypiszTak()
+    {
+        Console.WriteLine("Hello tak");
+    }
+
+    void wypiszNie()
+    {
+        Console.WriteLine("Hello nie");
+    }
+
+    public WritteHello(bool wypisz)
+    {
+        if (wypisz == true)
+        {
+            wypiszTak();
+        }
+        else
+        {
+            wypiszNie();
+        }
+    }
+}
