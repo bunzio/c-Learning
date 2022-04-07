f6423f57d6128869d42f9ffe13cd2d26255eb7e7
Bunzio <bunzeladam@gmail.com>
2022-04-07 14:20:35 +0200

    e

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿string dzialanie = "1234";
+//int[] liczby = new int[dzialanie.Length];
+char[] znak = new char[dzialanie.Length];
+//string[] splitDzialanie = new string[dzialanie.Length];
+
+/*
+ liczba[0] = Int32.Parse(str);
+ liczba[0] = liczba[0] - 1000;
+ Console.WriteLine(liczba[0]);
+ */
+#pragma warning disable CS8974 // Konwertowanie grupy metod na typ inny niż delegowany
+Console.WriteLine(Cos.returnCos);
+#pragma warning restore CS8974 // Konwertowanie grupy metod na typ inny niż delegowany
+
+
+for (int i = 0; i < dzialanie.Length; i++)
+{
+    //zmienne pomocnicze
+
+    bool czyZnak = !char.IsNumber(dzialanie[i]);
+    int ktoryZnak = 0;
+    
+     if (czyZnak)
+     {
+         znak[ktoryZnak] = dzialanie[i];
+         ktoryZnak++;
+     }
+    
+
+}
+
+class Cos
+{
+    public Cos(string txt)
+    {
+        Console.WriteLine(txt);
+    }
+
+    public static string returnCos()
+    {
+        string b = new string("sdadsadada");
+        return b;
+    }
+}
