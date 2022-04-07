15e33b4b2fd1acd996cda693cd9c89de22293207
Bunzio <bunzeladam@gmail.com>
2022-04-07 14:22:36 +0200

    xD

--- TestClasss/TestClasss/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using TestClasss;
-
-WritteHello p1 = new WritteHello(true);
-WritteHello p2 = new WritteHello(false);
-
-string p4 = Console.ReadLine();
-
-
