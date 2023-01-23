85a7a6f65572a298a3d10724790f1052400f31ad
Bunzio <bunzeladam@gmail.com>
2023-01-23 15:42:30 +0100

    tests and zad2_1

---
+++ Cwiczenia/Cwiczenia/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Cwiczenia;
+
+//Console.WriteLine("Hello, World!");
+
+zad2_1.zad();
---
+++ Cwiczenia/Cwiczenia/zad2.1.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.Design;
+using System.Data;
+
+namespace Cwiczenia;
+
+public static class zad2_1
+{
+    public static void zad()
+    {
+        Console.WriteLine("Program sprawdza czy trzy liczby tworza trójkę pitagorejską");
+        Console.WriteLine("Podal liczbę a:");
+        int a = int.Parse(Console.ReadLine());
+        Console.WriteLine("Podal liczbę a:");
+        int b = int.Parse(Console.ReadLine());
+        Console.WriteLine("Podal liczbę a:");
+        int c = int.Parse(Console.ReadLine());
+        Console.WriteLine("Wynik:  " + Check(a,b,c));
+    }
+    
+    private static bool Check(int a, int b, int c)
+    {
+        double count = Math.Pow(a,2) + Math.Pow(b,2);
+        Console.WriteLine(count);
+        bool resault = (count == Math.Pow(c,2));
+        return resault;
+    }
+}
